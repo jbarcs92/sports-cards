c1acfb41e9c3fb3b678a10eba872f6ef271df92d
jbarcs92 <jbarcl92@gmail.com>
2023-06-29 20:47:52 -0400

    Final commit

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
+
 require('dotenv').config();
 require('./config/database');
 
 var cardsRouter = require('./routes/cards');
 
 var app = express();
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
